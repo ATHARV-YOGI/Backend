9e417eb6f800fd39ce365eabed98ded090515df9
Atharv Yogi <atharvyogi123@gmail.com>
2026-01-06 14:39:58 +0530

    commit

---
+++ Javascript/script.js
@@ -0,0 +1,33 @@
+var arr=[1,2,3,4,"skdjf"];
+// arr.forEach(function(val){
+//     console.log(val + "hello");
+// })
+
+
+// var newarr = arr.map(function(val){
+//      return val+11;
+// })
+
+// console.log(newarr);
+
+
+// var newarr = arr.filter(function(val){
+//     if(val>3) {return true;}
+//     else return false;
+// })
+
+// console.log(newarr);
+
+
+// var newarr = arr.find(function(val){
+//     if(val==2)return val;
+// })
+
+// console.log(newarr);
+
+
+var obj = {
+    name:"sdlfk",
+    age:234
+}
+
