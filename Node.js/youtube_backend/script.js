7b78c5b0e81e15881af261f7d9c2615f953270bf
Atharv Yogi <atharvyogi123@gmail.com>
2026-01-06 18:40:59 +0530

    commit

--- Node.js/youtube_backend/script.js
+++ Node.js/youtube_backend/script.js
@@ -15,3 +15,18 @@ const fs = require('fs');
 //     if(err) console.error(err);
 //     else console.log("done");
 // })
+
+// fs.copyFile("hello.txt", "./copy/temp.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.unlink("hello.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+fs.rm("./copy", {recursive: true}, function(err){
+    if(err) console.error(err);
+    else console.log("removed");
+})
