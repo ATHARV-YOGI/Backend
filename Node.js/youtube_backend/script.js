30f3f4ef4e0578cbebc4f4b53f332da6c64b121c
Atharv Yogi <atharvyogi123@gmail.com>
2026-01-06 15:32:23 +0530

    commit

---
+++ Node.js/youtube_backend/script.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+// fs.writeFile("hey.txt", "hey hello ji", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+
+// fs.appendFile("hey.txt", " kaise ho", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.rename("hey.txt", "hello.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("done");
+// })
