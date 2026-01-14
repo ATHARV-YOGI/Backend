b217158c2854b25ff129dc3b8639bcdd4d4dc0b2
Atharv Yogi <atharvyogi123@gmail.com>
2026-01-14 11:31:43 +0530

    commit jwt2

---
+++ JWT/app.js
@@ -0,0 +1,14 @@
+const express =  require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+
+app.get("/", function (req, res){
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash("mypassword", salt, function(err, hash){
+               console.log(hash);
+        })
+    })
+})
+
+
+app.listen(3000);
